631c5eade3b5da06d872d2eb20de1e0ba883c629
Steven Degutis <sbdegutis@gmail.com>
2013-04-27 16:09:44 -0500

    that speclj version dont work yet

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   ;; TODO: i have no idea how many of these speclj things we actually need.
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [speclj "2.6.0"]]
+                 [speclj "2.5.0"]]
 
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]
                    :resource-paths ["dev/"]
