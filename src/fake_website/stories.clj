61484fde201cb15af9c146c527246ce65d5734c7
Steven Degutis <sbdegutis@gmail.com>
2013-04-27 17:01:22 -0500

    ooooh

--- src/fake_website/stories.clj
+++ src/fake_website/stories.clj
@@ -3,3 +3,6 @@
 ;; we should be able to read these step definitions and immediately so
 ;; "oh yeah i understand this perfectly" and have full assurrance and
 ;; confidence in the test suite.
+
+;; step definitions have to be THIN. not doing too much translation
+;; between the DSL and the real code. otherwise it becomes useless.
