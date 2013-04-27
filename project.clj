a410f6e86db56731a60f6e09abca293dceb69a70
Steven Degutis <sbdegutis@gmail.com>
2013-04-27 15:38:15 -0500

    deps

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
 (defproject stories "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "Freeform BDD lib for Clojure"
+  :url "https://github.com/sdegutis/stories"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+
+  ;; TODO: i have no idea how many of these speclj things we actually need.
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [speclj "2.6.0"]]
+
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]
+                   :resource-paths ["dev/"]
+                   :test-paths ["spec/"]}}
+
+  :plugins [[speclj "2.5.0"]]
+
+  )
