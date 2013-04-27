22b6140d2428201de51b27a15dc63b1a6ede4a98
Steven Degutis <sbdegutis@gmail.com>
2013-04-27 17:12:16 -0500

    this seems reasonable

--- spec/stories/basic_spec.clj
+++ spec/stories/basic_spec.clj
@@ -5,31 +5,40 @@
 (defsuite "carts"
 
   (defbackground
-    (create-product {:title "product #1"})
-    (create-product {:title "product #2"}))
+    (setup-product {:title "product #1" :price 200})
+    (setup-product {:title "product #2" :price 300}))
 
   (defstory "just visiting"
-    (As visitor)
-    (When (add-to-cart "product #1"))
-    (Then (= [] cart-contents)
-          (= :sign-in (redirected-to))))
+    (Given visitor (setup-visitor "larry"))
+    (When (add-to-cart larry "product #1"))
+    (Then (= [] (products-in-cart larry))
+          (= :sign-in (redirected-to larry))))
 
   (defstory "adding to your cart"
-    (As customer)
-    (When (add-to-cart "product #1"))
-    (Then (cart-has? "product #1")
-          (not (cart-has? "product #2"))))
+    (Given larry (setup-customer))
+    (When (add-to-cart larry "product #1"))
+    (Then (cart-has? larry "product #1")
+          (not (cart-has? larry "product #2"))))
 
   (defstory "removing from your cart"
-    (As customer)
-    (Given cart ["product #1" "product #2"])
-    (When (remove-from-cart "product #1"))
-    (Then (cart-has? "product #2")
-          (not (cart-has? "product #1"))))
+    (Given larry (setup-customer {:cart ["product #1" "product #2"]}))
+    (When (remove-from-cart larry "product #1"))
+    (Then (cart-has? larry "product #2")
+          (not (cart-has? larry "product #1"))))
+
+  (defstory "per-person carts"
+    (Given larry (setup-customer)
+           junior (setup-customer))
+    (When (add-to-cart larry  "product #1")
+          (add-to-cart junior "product #2"))
+    (Then (cart-has? larry  "product #1")
+          (cart-has? junior "product #2")
+          (not (cart-has? larry  "product #2"))
+          (not (cart-has? junior "product #1"))))
 
   (defstory "creating products"
-    (As admin)
-    (When (create-product {:title "product #3"}))
+    (Given bob (setup-admin))
+    (When (create-product bob {:title "product #3"}))
     (Then (= (map :title available-products)
              #{"product #1", "product #2", "product #3"})))
 
@@ -38,18 +47,15 @@
 (defsuite "refunds"
 
   (defbackground
-    (create-product {:title "product #1"})
-    (create-order {:title "product #2"})
-    )
+    (setup-product {:title "product #1" :price 200})
+    (setup-product {:title "product #2" :price 300}))
 
   (defstory "admins can refund payments"
-    (Given payment-1 (setup-paypal-payment 200)
-           order-1 (setup-order {:products ["product #1" "product #2"]
-                                 :payments [payment-1]})
-           customer (setup-customer {:orders [order-1]})
+    (Given customer (setup-customer {:products ["product #1" "product #2"]
+                                     :payments (setup-paypal-payment "abc" 500)})
            admin (setup-admin))
-    (When (refund-payment customer "product #1"))
-    (Then (has-product? customer "product #2")
-          (not (has-product "product #1"))))
+    (When (refund-payment admin customer "abc"))
+    (Then (= (owned-products customer)
+             [])))
 
   )
